923c240060ab0e34288aeeadb1b174a287a2ca0d
Alona7777 <abogolepova@gmail.com>
2023-12-20 14:01:42 -0800

    Update task_for_birthday.py

--- task_for_birthday.py
+++ task_for_birthday.py
@@ -1,4 +1,3 @@
-
 from collections import UserDict, defaultdict
 from datetime import date, datetime, timedelta
 from contact_book import AssistantBot, AddressBook, Record, Name, Phone, Birthday
@@ -26,35 +25,19 @@ def get_birthdays_per_week():
     assistent_bot = AssistantBot()
     date_today = date.today()          
     day_week_today = date_today.isoweekday() 
-
-    dec_31 = datetime(2024, 12, 31).date().replace(year=date_today.year)
-    dec_30 = datetime(2024, 12, 30).date().replace(year=date_today.year)  
-
     birthday_per_week = []
     for n, rec in assistent_bot.phone_book.data.items():
         name = n
         birth = rec.birthday.value.replace(year=date_today.year)
-        if date_today == datetime(2024, 1, 1).date().replace(year=date_today.year) and day_week_today == 1:
-            if birth == dec_30 or birth == dec_31:
-                birth = birth.replace(year=date_today.year-1)
-
-        if birth < date_today - timedelta(days=3):
+        if birth < date_today - timedelta(days=1):
             birth = birth.replace(year=date_today.year+1)
-        day_week = birth.isoweekday()
         
-        if day_week_today == 1:
-            start_date = date_today - timedelta(days=2)
-            end_date = date_today + timedelta(days=4)
-            if start_date <= birth <= end_date:
-                birthday_per_week.append([name, birth, day_week])
-
-        else:
-            end_date = date_today + timedelta(days=6)
-            if date_today <= birth <= end_date:
-                birthday_per_week.append([name, birth, day_week])
+        day_week = birth.isoweekday()
+        end_date = date_today + timedelta(days=7)
+        if date_today <= birth <= end_date:
+            birthday_per_week.append([name, birth, day_week])
     if len(birthday_per_week) == 0:
         return 'No Birthday this week'
-
     users = defaultdict(list)
     for item in birthday_per_week:
         if item[2] == 1 or item[2] == 6 or item[2] == 7:
@@ -67,6 +50,10 @@ def get_birthdays_per_week():
             users['Thursday'].append(item[0])
         if item[2] == 5:
             users['Friday'].append(item[0])
+        if item[2] == 6:
+            users['Satturday'].append(item[0])
+        if item[2] == 7:
+            users['Sunday'].append(item[0])
     return {key: value for key, value in users.items() if value} 
 
 
@@ -80,7 +67,7 @@ def birthday_in_given_days(value):
         name = n
         birth = rec.birthday.value.replace(year=date_today.year)
         if date_today <=  birth <= date_value:
-            contact_birth.append(name)
+            contact_birth.append(f'{name}: {rec.birthday.value}')
     if len(contact_birth) == 0:
         return 'No Birthday during this period'
     
@@ -96,5 +83,5 @@ if __name__ == "__main__":
     birthdays = get_birthdays_per_week()
     print(birthdays)
     
-    b = birthday_in_given_days(1)
+    b = birthday_in_given_days(100)
     print(b)
